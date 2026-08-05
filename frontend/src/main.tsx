4004b02b9d19c127ebcec5ce447d549c7bec8566
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-08-05 20:16:31 +0530

    toasted added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,19 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './providers/CartProvider';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <CartProvider>
         <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 2000,
+          }}
+        />
       </CartProvider>
     </BrowserRouter>
   </StrictMode>,
