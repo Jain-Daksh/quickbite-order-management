4ba2665f57fa5d8092f549ba4d47d50bcadf0545
Daksh jain <65433798+Jain-Daksh@users.noreply.github.com>
2026-08-05 17:27:12 +0530

    menu

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import TrackOrderPage from '../pages/track-order';
+import MenuPage from '../pages/Menu';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/menu' element={<MenuPage />} />
       <Route path='/track-order' element={<TrackOrderPage />} />
 
     </Routes>
